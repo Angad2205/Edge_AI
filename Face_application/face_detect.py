c8c2e495968d8e9fd4224c20f342185a6bc72489
Angad Singh <148006562+Angad2205@users.noreply.github.com>
2024-10-04 11:03:52 +0530

    Add files via upload
    updated file

--- Face_application/face_detect.py
+++ Face_application/face_detect.py
@@ -1,13 +1,26 @@
 import cv2 as cv
-import numpy as np 
-import os 
-#capture frame from the camera 
-cap = cv.VideoCapture(0)
-harcascade = cv.CascadeClassifier('har_cascade.xml')
-while True:
-    ret , frame = cap.read()
-    frame = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
-    cv.imshow('vedio' , frame)
+import numpy as np
+import os
 
-    if cv.waitKey(1) == ord('g'):
+cam = cv.VideoCapture(0)
+while True:
+    flag, frame = cam.read()
+    if not flag:
+        print("Failed to capture video")
         break
+
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    har_cascade = cv.CascadeClassifier('har_cascade.xml')
+    face = har_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=3)
+
+    for (x, y, w, h) in face:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), thickness=2)
+
+    cv.imshow('live video', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('g'):
+        break
+
+cam.release()
+cv.destroyAllWindows()
